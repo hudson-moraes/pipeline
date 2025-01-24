9d879668de085e7fc465bc2cd732dda37b55fec1
Hudson Santos de Moraes <dinhogugu_@hotmail.com>
2025-01-23 22:20:19 -0300

    fix: corrige mensagem de erro em soma Test

--- somaTest.php
+++ somaTest.php
@@ -9,7 +9,7 @@ Class testSoma {
         $class = new Soma();
 
         if ($class->somar(5,5) != 10) {
-            return throw new Exception('errado demais!');
+            return throw new Exception('errado demaiis!');
         }
 
         return 'aeeee isso mesmo!';
